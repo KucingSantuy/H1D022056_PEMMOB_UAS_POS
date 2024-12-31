a92bcffd105bdf6927c781924a7b2fcf35ea4d23
KucingSantuy <rafiattariq@gmail.com>
2024-12-31 20:38:20 +0700

    Update sidebar.dart

--- pos_flutter/lib/widgets/sidebar.dart
+++ pos_flutter/lib/widgets/sidebar.dart
@@ -2,21 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Sidebar extends StatelessWidget {
+  const Sidebar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         children: [
           ListTile(
-            title: Text("Dashboard"),
+            title: const Text("Dashboard"),
             onTap: () => Get.toNamed('/dashboard'),
           ),
           ListTile(
-            title: Text("Kasir"),
+            title: const Text("Kasir"),
             onTap: () => Get.toNamed('/cashier'),
           ),
           ListTile(
-            title: Text("Logout"),
+            title: const Text("Logout"),
             onTap: () => Get.offAllNamed('/login'),
           ),
         ],
